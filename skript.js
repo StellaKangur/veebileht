4ad0762bdd71d40c57e9369cfc559ad4cba485d2
unknown <triinmirjamtark@gmail.com>
2018-11-30 03:34:53 +0200

    lisasin javascriptiga ühe närvidele käiva popup akna

--- skript.js
+++ skript.js
@@ -1,1 +1,11 @@
 //dünaamilisuse loomiseks teeks lahtri "kirjuta meile"
+
+
+	function trigger()
+	{
+	document.getElementById("hover").addEventListener("mouseover", popup);
+	function popup()
+	{
+	alert("Aitäh, et tunned huvi meie teenuste vastu!");
+	}
+	}
